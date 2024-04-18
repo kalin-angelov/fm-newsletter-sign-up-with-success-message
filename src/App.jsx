879d042bd593fddc8e989807c806e4aee68733db
Kalizar <kalinangelov@abv.bg>
2024-04-18 12:33:10 +0300

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import "./App.css"
 
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import Message from "./components/Message/Message";
+import { Context } from "./context/Context";
 
 function App() {
+  const [email, setEmail] = useState('');
+  const contextValue = {
+    email,
+    setEmail
+  };
+
   return (
-    <>
-      <Main />
-      {/* <Footer /> */}
-    </>
+    <Context.Provider value={contextValue} >
+      <>
+        <Routes>
+          <Route path="/" element={<Main />}/>
+          <Route path="/success" element={<Message />} />
+        </Routes>
+        <Footer />
+      </>
+    </Context.Provider>
   );
 };
 
