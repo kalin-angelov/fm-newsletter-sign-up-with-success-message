1c33cd1609437a1bae41035c099823075d3f9137
Kalizar <kalinangelov@abv.bg>
2024-04-18 12:32:13 +0300

    add style to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
+import styles from "./Footer.module.css";
+
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="https://www.frontendmentor.io/profile/kalin-angelov">Kalin Angelov</a>.
+        Coded by <a href="https://www.frontendmentor.io/profile/kalin-angelov" target="_blank">Kalin Angelov</a>.
     </footer>
   );
 };
