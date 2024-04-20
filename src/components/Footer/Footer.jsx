b19ebe8068a36d28c3f9e49ad2735de137128003
Kalizar <kalinangelov@abv.bg>
2024-04-20 15:02:23 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="https://www.frontendmentor.io/profile/kalin-angelov" target="_blank">Kalin Angelov</a>.
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="https://www.frontendmentor.io/profile/kalin-angelov" target="_blank">Kalin Angelov</a>.
     </footer>
   );
 };
